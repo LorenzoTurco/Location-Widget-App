1aacd576a7cd3be3b132c9cb559b9ab913bb644d
LorenzoTurco <l.turco@se19.qmul.ac.uk>
2022-09-27 19:45:35 +0100

    moved cities data to separate js file

---
+++ location-widget-app/src/data/cities.js
@@ -0,0 +1,25 @@
+const cities = [
+  "London",
+  "Madrid",
+  "Lisbon",
+  "Rome",
+  "Sofia",
+  "Paris",
+  "Vienna",
+  "Milan",
+  "Athens",
+  "Chisinau",
+  "Budapest",
+  "Copenhagen",
+  "Helsinky",
+  "Dublin",
+  "Belfast",
+  "Minsk",
+  "Nuuk",
+  "Oslo",
+  "Luxemburg",
+  "Podgorica",
+  "Riga",
+];
+
+export default cities;
