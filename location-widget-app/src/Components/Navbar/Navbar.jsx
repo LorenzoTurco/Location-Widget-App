ad5d1a864d4bd96c09e4955261e00a023add6902
LorenzoTurco <l.turco@se19.qmul.ac.uk>
2022-09-28 12:54:20 +0100

    a greeting will now be displayed depending on the time of day.

--- location-widget-app/src/Components/Navbar/Navbar.jsx
+++ location-widget-app/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
 import "./Navbar.scss";
+import { useState, useEffect } from "react";
+import { getYourLocationTimeInHours } from "../../api/api";
 
 const Navbar = ({ score }) => {
+  const [hour, setHour] = useState(null);
+  const [greeting, setGreeting] = useState("");
+
+  const findGreeting = () => {
+    if (hour >= 5 && hour < 12) return "Good Morning";
+    if (hour > 12 && hour <= 6) return "Good Afternoon";
+    return "Good Evening";
+  };
+
+  useEffect(() => {
+    getYourLocationTimeInHours().then((res) => setHour(res));
+    setGreeting(findGreeting());
+  }, []);
+
   return (
     <div className="nav">
       <h1>Score: {score}</h1>
       <p>
-        Guess the place with the <big className="nav__higher-text">higher</big>{" "}
-        temperature
+        {greeting}! - Guess the place with the{" "}
+        <big className="nav__higher-text">higher</big> temperature
       </p>
     </div>
   );
